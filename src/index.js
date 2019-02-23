ebe78fa17a411fa5f15ff2000eaec5c3353133e9
Connor Callahan <connorwcallahan@gmail.com>
2019-02-23 13:57:54 -0500

    reducer fix

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
-import rootReducer from './reducers'
+import reducer from './reducers'
 import {Provider} from 'react-redux'
 
-const store = createStore(rootReducer)
+const store = createStore(reducer)
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
---
+++ src/reducers.js
@@ -0,0 +1,45 @@
+
+const defaultState = {
+  symbols: [],
+  stockFilter: '',
+  filtered: [],
+  stock: null,
+  profile: null,
+  chart: null,
+  icon: null,
+  newsFeed: [],
+  login: false,
+  username: null,
+  password: null
+}
+
+export default(state=defaultState, action) => {
+
+  switch(action.type) {
+    case 'FETCH_SYMBOLS':
+    return {...state, symbols: action.payload}
+    case 'HANDLE_STOCK_FILTER':
+    return {...state, stockFilter: action.payload}
+    case 'FILTER_SYMBOLS':
+    return {...state, filtered: action.payload}
+    case 'HANDLE_STOCK_DATA':
+    return {...state, stock: action.payload}
+    case 'HANDLE_STOCK_PROFILE':
+    return {...state, profile: action.payload}
+    case 'HANDLE_STOCK_CHART':
+    return {...state, chart: action.payload}
+    case 'HANDLE_STOCK_ICON':
+    return {...state, icon: action.payload}
+    case 'HANDLE_NEWS_FEED':
+    return {...state, newsFeed: action.payload}
+    case 'HANDLE_USER_LOGIN':
+    return {...state, login: action.payload}
+    case 'HANDLE_USERNAME':
+    return {...state, username: action.payload}
+    case 'HANDLE_PASSWORD':
+    return {...state, password: action.payload}
+    default:
+
+    return state
+  }
+}
